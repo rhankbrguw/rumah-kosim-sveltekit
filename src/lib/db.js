b7632d1515495a157c6d5d3d289f1309a02e3bed
Raihan Akbar Gunawan <raihanakbarg28@gmail.com>
2025-06-24 08:21:19 +0700

    Final preparation for Railway deployment

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,8 @@
 import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { DATABASE_URL } from '$env/static/private';
 
 const pool = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME
+	uri: DATABASE_URL
 });
 
 export async function query(sql, params) {
